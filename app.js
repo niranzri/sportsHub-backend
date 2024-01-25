42dd6fb4ae71e9259cadfd60b5aadd7b1e98b7c9
Marine Le Cleach <lecleach.m@gmail.com>
2024-01-25 17:43:17 +0100

    add user routes

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ app.use("/api", indexRoutes);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+const userRoutes = require("./routes/user.routes");
+app.use("/profile", userRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
---
+++ routes/user.routes.js
@@ -0,0 +1,74 @@
+const router = require("express").Router();
+const { isAuthenticated } = require("../middlewares/route-guard.middleware");
+const User = require("../models/User.model");
+
+//GET all
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find().populate("favourites");
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error while getting users" });
+  }
+});
+
+// GET one
+router.get("/:userId", async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const oneUser = await User.findById(userId);
+    res.status(200).json(oneUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error while getting the user" });
+  }
+});
+
+// PUT one
+router.put(
+  "/:userId",
+  /*isAuthenticated,*/ async (req, res) => {
+    // const { userId } = req.tokenPayload;
+    const payload = req.body;
+    const { currentUserId } = req.params;
+    try {
+      const userToUpdate = await User.findById(currentUserId);
+      //   if (userToUpdate._Id == userId) {
+      const updatedUser = await User.findByIdAndUpdate(currentUserId, payload, {
+        new: true,
+      });
+      res.status(200).json(updatedUser);
+      /* } else {
+        res.status(403).json({ message: "you are not the right user" });
+      }*/
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "error while updating the user" });
+    }
+  }
+);
+
+// DELETE one
+router.delete(
+  "/:userId",
+  /*isAuthenticated,*/ async (req, res) => {
+    //  const { userId } = req.tokenPayload;
+    const { currentUserId } = req.params;
+    try {
+      const userToDelete = await User.findById(currentUserId);
+
+      //  if (userToDelete.company == userId) {
+      console.log("Deleting");
+      await User.findByIdAndDelete(currentUserId);
+      res.status(204).json();
+      /* } else {
+        res.status(403).json({ message: "you are not the right user" });
+      }*/
+    } catch (error) {
+      res.status(500).json({ message: "error while deleting the book" });
+    }
+  }
+);
+
+module.exports = router;
