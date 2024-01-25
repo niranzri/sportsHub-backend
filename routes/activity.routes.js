7fd68e3e8683116b5fcfa173ea125590757b022d
Marine Le Cleach <lecleach.m@gmail.com>
2024-01-25 15:51:31 +0100

    finish activity route

--- routes/activity.routes.js
+++ routes/activity.routes.js
@@ -39,4 +39,45 @@ router.post("/", async (req, res) => {
   }
 });
 
+// PUT one
+router.put("/:activityId", isAuthenticated, async (req, res) => {
+  // const { userId } = req.tokenPayload
+  const payload = req.body;
+  const { activityId } = req.params;
+  try {
+    const activityToUpdate = await Activity.findById(activityId);
+    //if (activityToUpdate.companyId == userId) {
+    const updatedActivity = await Activity.findByIdAndUpdate(
+      activityId,
+      payload,
+      { new: true }
+    );
+    res.status(200).json(updatedActivity);
+    /* } else {
+      res.status(403).json({ message: 'you are not the right user' })
+    }*/
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error while updating the activity" });
+  }
+});
+// DELETE one
+router.delete("/:activityId", isAuthenticated, async (req, res) => {
+  // const { userId } = req.tokenPayload
+  const { activityId } = req.params;
+  try {
+    const activityToDelete = await Activity.findById(activityId);
+
+    //if (activityToDelete.companyId == userId) {
+    console.log("Deleting");
+    await Activity.findByIdAndDelete(activityId);
+    res.status(204).json();
+    /* } else {
+      res.status(403).json({ message: 'you are not the right user' })
+    }*/
+  } catch (error) {
+    res.status(500).json({ message: "error while deleting the book" });
+  }
+});
+
 module.exports = router;
