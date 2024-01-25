ed0174c975e0d7a038210af9970d6c3c45129850
Marine Le Cleach <lecleach.m@gmail.com>
2024-01-25 14:12:23 +0100

    add all routes

--- db/index.js
+++ db/index.js
@@ -6,7 +6,8 @@ const mongoose = require("mongoose");
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/sports-hub";
+  process.env.MONGODB_URI ||
+  "mongodb+srv://ironhacker:KkYKhXCQqdZhsj0C@sports-hub.lpqtfsu.mongodb.net/sports-hub";
 
 const withDB = async (serverListener) => {
   try {
