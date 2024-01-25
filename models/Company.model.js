90d40a66181669080aeea8767a70fa60259b0981
Noelia <noeliairanzoribera@gmail.com>
2024-01-25 16:26:59 +0100

    changed type employee in model

--- models/Company.model.js
+++ models/Company.model.js
@@ -1,14 +1,4 @@
-/*Name: string
-
-City: string
-
-Address: string
-
-Image: img
-
-Employee: array of strings (link to User model - aka ids)*/
-
-const { Schema, model } = require("mongoose");
+const { Schema, Types, model } = require("mongoose");
 
 const companySchema = new Schema({
   name: { type: String, required: true, lowercase: true },
@@ -16,7 +6,7 @@ const companySchema = new Schema({
   address: { type: String },
   postcode: { type: String }, 
   image: { type: String },
-  employee: { type: [String], ref: "User" },
+  employee: [{ type: Types.ObjectId, ref: "User" }],
 });
 
 const Company = model("Company", companySchema);
